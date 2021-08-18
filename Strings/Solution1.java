5ef5603e9241a8d250a1a25c21c035b08373dba9
HarshShiyani <56556849+HarshShiyani@users.noreply.github.com>
2021-08-18 13:37:30 +0530

    Create Solution1.java

---
+++ Strings/Solution1.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class Solution1 {
+
+	public static void main(String[] args) {
+		String s = "Java Exercises!";
+		
+		System.out.println(s.charAt(0));
+		System.out.println(s.charAt(10));
+	}
+
+}
